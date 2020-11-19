5558c1313e7a6550ccacd1ac13e9bbecd1f11707
gunkarlsson <hi@gunkarlsson.com>
2020-11-19 11:33:26 +0100

    ✨ added creation date and last saved date

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
 // VARIABLES
-window.localStorage.clear()
+window.localStorage.clear();
 const textarea = document.querySelector("#textarea");
 const newDoc = document.querySelector("#new-doc");
-const deleteDocButton = document.querySelector('#delete-doc');
+const deleteDocButton = document.querySelector("#delete-doc");
 
 // OBJECTS
-const docData = { id:'',title:'', content:'', favorite: true, creationDate:'', lastSavedDate:'', tags:[],};
+const docData = {
+  id: "",
+  title: "",
+  content: "",
+  favorite: true,
+  creationDate: "",
+  lastSavedDate: "",
+  tags: [],
+};
 
 // EVENTLISTENERS
 newDoc.addEventListener("click", () => {
   console.log("click");
 });
 
-deleteDocButton.addEventListener('click', () => {
-  textarea.value = '';
+deleteDocButton.addEventListener("click", () => {
+  textarea.value = "";
 
-  deleteDoc()
+  deleteDoc();
 });
 
 // Input event
 textarea.addEventListener("input", (e) => {
   // input value in textarea,
-  saveDoc()
+  saveDoc();
   // koden som ska sparas till local storage
 });
 
 // FUNCTIONS
-function saveDoc () {
+function saveDoc() {
   console.log(textarea.value);
   docData.content = textarea.value;
-window.localStorage.setItem('key', JSON.stringify(docData))
+  //kollar om det finns ett creation date, om inte så skapar den datum)
+  if (!docData.creationDate) {
+    docData.creationDate = new Date();
+  }
+
+  docData.lastSavedDate = new Date();
+  window.localStorage.setItem("1", JSON.stringify(docData));
 }
 
-function deleteDoc () {
-  window.localStorage.removeItem('key')
-} 
+function deleteDoc() {
+  window.localStorage.removeItem("1");
+}
