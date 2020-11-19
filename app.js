278ac8a6b209a3d8f64d3a3cb5ed56cdad5a66b8
patrik-fredlund <patrikfredlunds@gmail.com>
2020-11-19 10:56:26 +0100

    :sparkles: added feature to create save and delete note

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 // VARIABLES
+window.localStorage.clear()
 const textarea = document.querySelector("#textarea");
 const newDoc = document.querySelector("#new-doc");
+const deleteDocButton = document.querySelector('#delete-doc');
+
+// OBJECTS
+const docData = { id:'',title:'', content:'', favorite: true, creationDate:'', lastSavedDate:'', tags:[],};
 
 // EVENTLISTENERS
 newDoc.addEventListener("click", () => {
   console.log("click");
 });
 
+deleteDocButton.addEventListener('click', () => {
+  textarea.value = '';
+
+  deleteDoc()
+});
+
 // Input event
 textarea.addEventListener("input", (e) => {
   // input value in textarea,
+  saveDoc()
   // koden som ska sparas till local storage
-  console.log(textarea.value);
 });
 
 // FUNCTIONS
+function saveDoc () {
+  console.log(textarea.value);
+  docData.content = textarea.value;
+window.localStorage.setItem('key', JSON.stringify(docData))
+}
+
+function deleteDoc () {
+  window.localStorage.removeItem('key')
+} 
