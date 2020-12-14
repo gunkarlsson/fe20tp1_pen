ffa58899367e54eaf9dda5b124bb05d529bba2ae
mrautenberg <max.rautenberg89@gmail.com>
2020-12-14 14:44:56 +0100

    💩 🚧 moved menu to bottom (still bad)

--- app.js
+++ app.js
@@ -16,62 +16,62 @@ const textarea = new SimpleMDE({
     },
     {
       name: "heading",
-      action: SimpleMDE.toggleItalic,
+      action: SimpleMDE.toggleHeader,
       className: "fa fa-header",
       title: "Heading",
     },
-    "|",
-    {
-      name: "quote",
-      action: SimpleMDE.toggleBlockquote,
-      className: "fa fa-quote-left",
-      title: "Quote",
-    },
+    // "|",
+    // {
+    //   name: "quote",
+    //   action: SimpleMDE.toggleBlockquote,
+    //   className: "fa fa-quote-left",
+    //   title: "Quote",
+    // },
     {
       name: "unordered-list",
       action: SimpleMDE.toggleUnorderedList,
       className: "fa fa-list-ul",
       title: "Generic List",
     },
-    {
-      name: "ordered-list",
-      action: SimpleMDE.toggleOrderedList,
-      className: "fa fa-list-ol",
-      title: "Numbered List",
-    },
-    "|",
-    {
-      name: "link",
-      action: SimpleMDE.drawLink,
-      className: "fa fa-link",
-      title: "Create Link",
-    },
+    // {
+    //   name: "ordered-list",
+    //   action: SimpleMDE.toggleOrderedList,
+    //   className: "fa fa-list-ol",
+    //   title: "Numbered List",
+    // },
+    // "|",
+    // {
+    //   name: "link",
+    //   action: SimpleMDE.drawLink,
+    //   className: "fa fa-link",
+    //   title: "Create Link",
+    // },
     {
       name: "image",
       action: SimpleMDE.drawImage,
       className: "fa fa-picture-o",
       title: "Insert Image",
     },
-    "|",
+    // "|",
     {
       name: "preview",
       action: SimpleMDE.togglePreview,
       className: "fa fa-eye no-disable",
       title: "Toggle Preview",
     },
-    {
-      name: "side-by-side",
-      action: SimpleMDE.toggleSideBySide,
-      className: "fa fa-columns no-disable no-mobile",
-      title: "Toggle Side by Side",
-    },
-    {
-      name: "fullscreen",
-      action: SimpleMDE.toggleFullScreen,
-      className: "fa fa-arrows-alt no-disable no-mobile",
-      title: "Toggle Fullscreen",
-    },
-    "|",
+    // {
+    //   name: "side-by-side",
+    //   action: SimpleMDE.toggleSideBySide,
+    //   className: "fa fa-columns no-disable no-mobile",
+    //   title: "Toggle Side by Side",
+    // },
+    // {
+    //   name: "fullscreen",
+    //   action: SimpleMDE.toggleFullScreen,
+    //   className: "fa fa-arrows-alt no-disable no-mobile",
+    //   title: "Toggle Fullscreen",
+    // },
+    // "|",
     {
       name: "downloadNote",
       action: function downloadNote() {
@@ -89,7 +89,6 @@ const textarea = new SimpleMDE({
     {
       name: "print",
       action: function printNote() {
-        console.log("click");
         document.querySelector(".CodeMirror").style.border = "none";
         document.querySelector(".editor-toolbar").style.border = "none";
 
